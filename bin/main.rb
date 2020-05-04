47e91b384790035b7daae36363af2ddad53e4b92
Moin <kmoin786@gmail.com>
2020-05-05 00:36:03 +0530

    fix rubocop errors except methodLength

--- bin/main.rb
+++ bin/main.rb
@@ -10,22 +10,24 @@ def scraper
   url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=#{country}"
   unparsed_page = RestClient.get(url)
   parsed_page = Nokogiri::XML(unparsed_page)
-  items = parsed_page.xpath("//item")
-  news = items.xpath("//ht:news_item[1]/ht:news_item_title[1]")
-  news_url = items.xpath("//ht:news_item[1]/ht:news_item_url[1]")
+  items = parsed_page.xpath('//item')
+  news = items.xpath('//ht:news_item[1]/ht:news_item_title[1]')
+  news_url = items.xpath('//ht:news_item[1]/ht:news_item_url[1]')
   puts
   p "Top #{t} Trends in #{country}"
   puts
   t.times do |i|
-    p "#{i+1} #{CGI.unescapeHTML(news[i].text)}"
+    p "#{i + 1} #{CGI.unescapeHTML(news[i].text)}"
   end
   puts
   p "To access any news, please enter the number displayed beside the title OR press 'm' for menu"
   user_in = gets.chomp
-  user_num=Integer(user_in) rescue false
-  if user_num
-    system(OS.open_file_command, news_url[user_in.to_i-1].text)
-  end
+  user_num =
+    begin
+      Integer(user_in)
+    rescue StandardError
+      false
+    end
+  system(OS.open_file_command, news_url[user_in.to_i - 1].text) if user_num
 end
 scraper
-
