ae81405d7fb585db9699e63e6dbac5c10e7473af
Moin <kmoin786@gmail.com>
2020-05-05 03:16:04 +0530

    fix linter errors with exception of complexity error

--- bin/main.rb
+++ bin/main.rb
@@ -12,6 +12,7 @@ def menu
   p '2. Show Trends from a different country'
   p '3. Show stories'
   p '4. Reset country and number of trend'
+  p '5. Quit'
   user_i_m = gets.chomp
   if num?(user_i_m)
     case user_i_m.to_i
@@ -19,7 +20,7 @@ def menu
       p 'Please enter the number of stories you want to view'
       new_t = gets.chomp
       if num?(new_t) && new_t.to_i <= @news.size
-        @t = new_t.to_i 
+        @t = new_t.to_i
         start
       else
         menu
@@ -28,13 +29,15 @@ def menu
       p 'Please enter the country name'
       new_country = gets.chomp
       @country = Geocoder.search(new_country).first.country_code.upcase
-      p "country changed to #{@country}!"
+      p "country changed to #{Geocoder.search(new_country).first.country}!"
       menu
     when 3
       start
     when 4
       ini
       start
+    when 5
+      p 'Thank you for using Trend Search! Good day!'
     end
   else
     p 'kayum'
@@ -42,16 +45,16 @@ def menu
 end
 
 def num?(i)
-  begin
-    Integer(i)
-  rescue StandardError
-    false
-  end
+  Integer(i)
+rescue StandardError
+  false
 end
+
 def ini
   @country = 'US'
   @t = 5
 end
+
 def start
   url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=#{@country}"
   unparsed_page = RestClient.get(url)
@@ -65,21 +68,22 @@ def start
   @t.times do |i|
     p "#{i + 1}. #{CGI.unescapeHTML(@news[i].text)}"
     begin
-      EasyTranslate.api_key = ENV['TRANSLATE_KEY']
+      EasyTranslate.api_key = ENV['TRANSLATE_EY']
       if EasyTranslate.detect(CGI.unescapeHTML(@news[i].text)) != 'en'
-        
-        p "^Translation: ##{EasyTranslate.translate(CGI.unescapeHTML(@news[i].text), :to => 'en')}"
+
+        p "^Translation: ##{EasyTranslate.translate(CGI.unescapeHTML(@news[i].text), to: 'en')}"
         puts
       end
-    rescue
+    rescue StandardError
+      puts
     end
   end
   puts
-  p "To access any news, please enter the number displayed beside the title" 
+  p 'To access any news, please enter the number displayed beside the title'
   p "OR press 'm' for menu or 'q' to quit"
   user_in = gets.chomp
   user_num = num?(user_in)
-    
+
   if user_num
     system(OS.open_file_command, news_url[user_in.to_i - 1].text)
   else
